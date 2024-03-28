ec381dbcd75389b8b727f8307366605a92733e52
Yunus YAVUZ <yunusyavuz016@gmail.com>
2024-03-28 18:27:20 +0300

    Footer Test

--- src/screens/HomeScreen/components/HomeListFooter/__tests__/index.tsx
+++ src/screens/HomeScreen/components/HomeListFooter/__tests__/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react-native';
+import {fireEvent, render} from '@testing-library/react-native';
 import Footer from '..';
 
 describe('Footer', () => {
@@ -7,24 +7,24 @@ describe('Footer', () => {
   const mockHandleNextPage = jest.fn();
 
   test('renders correctly with page number', () => {
-    const { getByText } = render(
+    const {getByText} = render(
       <Footer
         pageNumber={1}
         handlePreviousPage={mockHandlePreviousPage}
         handleNextPage={mockHandleNextPage}
-      />
+      />,
     );
 
     expect(getByText('Sayfa: 1')).toBeTruthy();
   });
 
   test('handles previous page click correctly', () => {
-    const { getByText } = render(
+    const {getByText} = render(
       <Footer
         pageNumber={2}
         handlePreviousPage={mockHandlePreviousPage}
         handleNextPage={mockHandleNextPage}
-      />
+      />,
     );
 
     fireEvent.press(getByText('Geri'));
@@ -32,12 +32,12 @@ describe('Footer', () => {
   });
 
   test('handles next page click correctly', () => {
-    const { getByText } = render(
+    const {getByText} = render(
       <Footer
         pageNumber={1}
         handlePreviousPage={mockHandlePreviousPage}
         handleNextPage={mockHandleNextPage}
-      />
+      />,
     );
 
     fireEvent.press(getByText('İleri'));
@@ -45,15 +45,14 @@ describe('Footer', () => {
   });
 
   test('disables previous button when page number is 1', () => {
-    const { getByText } = render(
+    const {getByText} = render(
       <Footer
         pageNumber={1}
         handlePreviousPage={mockHandlePreviousPage}
         handleNextPage={mockHandleNextPage}
-      />
+      />,
     );
-
-    const previousButton = getByText('Geri');
-    expect(previousButton.props.isDisabled).toBeTruthy();
+    const previousButton = getByText('Geri').parent?.parent;
+    expect(previousButton?.props.accessibilityState.disabled).toBeTruthy();
   });
 });
