b5a890bdeb6821969d5661bb55e5a8dd43f86f15
Yunus YAVUZ <yunusyavuz016@gmail.com>
2024-03-28 23:35:28 +0300

    remove dark mode

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import { SafeAreaView, StatusBar } from 'react-native';
+import ErrorBoundary from 'react-native-error-boundary';
 import HomeScreen from './src/screens/HomeScreen';
 import {
   COLORS,
@@ -9,14 +8,9 @@ import {
   heightStyles,
   widthStyles,
 } from './src/shared/styles';
-import ErrorBoundary from 'react-native-error-boundary';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+ 
 
   return (
     <ErrorBoundary>
